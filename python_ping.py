f5d5b8660b716f0d08cd10ec838ff8bd82ac3f9a
LinuxmanR4 <inguanzo@gmail.com>
2022-04-01 11:46:11 -0600

    Create python_ping.py

---
+++ python_ping.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+# python_ping.py
+#
+# Una sencilla herramienta para verificar algunos elementos 
+# de una red usando python y fping.
+# ---------------------------------------------------------
+# Este programa es software libre. Puede redistribuirlo y/o
+# modificarlo bajo los términos de la Licencia Pública General
+# de GNU según es publicada por la Free Software Foundation,
+# bien de la versión 2 de dicha Licencia o bien (según su
+# elección) de cualquier versión posterior.
+#
+# Este programa se distribuye con la esperanza de que sea
+# útil, pero SIN NINGUNA GARANTÍA, incluso sin la garantía
+# MERCANTIL implícita o sin garantizar la CONVENIENCIA PARA UN
+# PROPÓSITO PARTICULAR. Para más detalles, véase la Licencia
+# Pública General de GNU.
+#
+# Autor: LinuxmanR4
+# https://linuxmanr4.com
+#
